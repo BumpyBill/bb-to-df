59ce6daf0a7285c24007dec70020c748b537c6aa
Lachie-Source <62362511+Lachie-Source@users.noreply.github.com>
2021-02-13 21:22:24 +1100

    "Hello, World" Mod.

---
+++ src/main/java/me/bumpybill/bbdf/Main.java
@@ -0,0 +1,12 @@
+package me.bumpybill.bbdf;
+
+import net.fabricmc.api.ModInitializer;
+
+import java.io.Console;
+
+public class Main implements ModInitializer {
+    @Override
+    public void onInitialize() {
+        System.out.println("Hello Fabric world!");
+    }
+}
